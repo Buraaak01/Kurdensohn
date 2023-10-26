49857f6bd93b14dc67fdb56cba60ca3af869f78e
Buraaak01 <sfi.dogan@gmail.com>
2023-10-26 19:32:21 +0200

    refactor

--- bot.js
+++ bot.js
@@ -13,21 +13,22 @@ const client = new Client({
   ]
 });
 
+// ------------------------------ EVENT LISTENER -----------------------------
+
 client.on("ready", () => {
   console.log("Kurdensohn ist ready");
 });
 
 client.on("messageCreate", async (message) => {
   isHurensohn(message) && message.reply("Du Hurensohn");
-
   isUnderage(message) &&
     message.reply(
       "CAUTION⚠️! THIS PERSON IS UNDERAGE. DO NOT, I REPEAT, DO NOT SEND THEM UR PICTURES"
     );
-
   isBestie(message) && message.react("♥");
 });
 
+// ------------------------------ UTIL -----------------------------
 function isHurensohn(message) {
   return message.member.roles.cache.find(
     (role) => role.name === "Opfer von Kurdensohn"
@@ -35,6 +36,7 @@ function isHurensohn(message) {
     ? true
     : false;
 }
+
 function isUnderage(message) {
   return message.member.roles.cache.find(
     (role) => role.id === "1166883388490268682"
@@ -42,9 +44,11 @@ function isUnderage(message) {
     ? true
     : false;
 }
+
 function isBestie(message) {
   return message.member.roles.cache.find((role) => role.name === "bestie")
     ? true
     : false;
 }
+
 client.login(process.env.TOKEN);
